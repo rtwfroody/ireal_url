ba2b65124c983786fe835ecab850e2c8b6b70780
Tim Newsome <tim@casualhacker.net>
2023-10-05 17:41:45 -0700

    Basically works, and there's a test that passes.
    Plenty more parsing that could be done, but it's good enough for me
    right now.

---
+++ src/lib.rs
@@ -0,0 +1,361 @@
+mod tokenize;
+pub use tokenize::{
+    Bar,
+    BarElement,
+    Chord,
+    Music,
+    Note,
+    Number,
+    Flavor
+};
+
+const MUSIC_PREFIX : &str = "1r34LbKcu7";
+
+/*
+ * Format reference: https://github.com/pianosnake/ireal-reader
+ */
+
+fn unscramble(mut text : &str) -> String {
+    /* Directly translated from
+     * https://github.com/pianosnake/ireal-reader/blob/ce643f069732ab93b1dcbd621b6c0edfe9ab8a8b/unscramble.js#L5 */
+    let mut result = String::new();
+
+    while text.len() > 50 {
+        let (part, remainder) = text.split_at(50);
+        text = remainder;
+
+        if text.len() < 2 {
+            result.push_str(part);
+        } else {
+            result.push_str(&obfusc50(part));
+        }
+    }
+
+    result.push_str(text);
+    result
+}
+
+fn obfusc50(text : &str) -> String {
+    /* Directly translated from
+     * https://github.com/pianosnake/ireal-reader/blob/ce643f069732ab93b1dcbd621b6c0edfe9ab8a8b/unscramble.js#L21 */
+
+    // The first 5 characters are switched with the last 5.
+    let mut chars : Vec<char> = text.chars().collect();
+    let last = chars.len() - 1;
+    for i in 0..5 {
+        chars.swap(i, last - i);
+    }
+    // Characters 10-24 are also switched.
+    for i in 10..24 {
+        chars.swap(i, last - i);
+    }
+
+    chars.into_iter().collect::<String>()
+}
+
+fn decode_music(text : &str) -> Result<Music, String> {
+    if !text.starts_with(MUSIC_PREFIX) {
+        return Err(format!("Music doesn't start with {}", MUSIC_PREFIX));
+    }
+    let unscrambled = unscramble(&text[MUSIC_PREFIX.len()..]);
+    tokenize::tokenize_music(unscrambled.as_str())
+}
+
+fn hex_digit_value(ch : char) -> Result<u32, String> {
+    match ch {
+        '0' => Ok(0), '1' => Ok(1), '2' => Ok(2), '3' => Ok(3),
+        '4' => Ok(4), '5' => Ok(5), '6' => Ok(6), '7' => Ok(7),
+        '8' => Ok(8), '9' => Ok(9), 'a' => Ok(10), 'b' => Ok(11),
+        'c' => Ok(12), 'd' => Ok(13), 'e' => Ok(14), 'f' => Ok(15),
+        'A' => Ok(10), 'B' => Ok(11), 'C' => Ok(12), 'D' => Ok(13),
+        'E' => Ok(14), 'F' => Ok(15),
+        _ => Err(format!("Unknown value: {}", ch))
+    }
+}
+
+fn unescape_percent(text : &str) -> Result<String, String>
+{
+    enum UnescapeState {
+        Plain,
+        Percent,
+        One
+    }
+
+    let mut state = UnescapeState::Plain;
+    let mut result = String::new();
+    let mut num = 0;
+    for c in text.chars() {
+        match state {
+            UnescapeState::Plain => match c {
+                '%' => state = UnescapeState::Percent,
+                _ => result.push(c)
+            },
+            UnescapeState::Percent => {
+                num = 16 * hex_digit_value(c)?;
+                state = UnescapeState::One
+            }
+            UnescapeState::One => {
+                num += hex_digit_value(c)?;
+                result.push(char::from_u32(num).unwrap());
+                state = UnescapeState::Plain
+            }
+        }
+    }
+    Ok(result)
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Collection {
+    pub title : String,
+    pub songs : Vec<Song>
+}
+
+#[derive(Debug, PartialEq, Clone, Eq)]
+pub struct Song {
+    pub title : String,
+    pub composer : String,
+    pub style : String,
+    pub key : String,
+    pub transpose : String,
+    pub music : Music,
+    pub comp_style : String,
+    pub bpm : u32,
+    pub repeats : String
+}
+
+impl Song {
+    fn from_text(text : &str) -> Self {
+        let parts : Vec<&str> = text.split("=").collect();
+        println!("title: {}", parts[0]);
+        Song {
+            title: parts[0].to_string(),
+            composer: parts[1].to_string(),
+            style: parts[3].to_string(),
+            key: parts[4].to_string(),
+            transpose: parts[5].to_string(),
+            music: decode_music(parts[6]).unwrap(),
+            comp_style: parts[7].to_string(),
+            bpm: parts[8].parse().unwrap(),
+            repeats: parts[9].to_string()
+        }
+    }
+}
+
+/* See https://loophole-letters.vercel.app/ireal-changes */
+pub fn parse_url(mut text : &str) -> Result<Collection, String> {
+    text = text.trim();
+    if !text.starts_with("irealb://") {
+        return Err("Expected URL to start with 'irealb://'".to_string())
+    }
+
+    let unescaped = unescape_percent(&text[9..])?;
+
+    let mut parts : Vec<&str> = unescaped.split("===").collect();
+    let collection_title =
+            if parts.len() > 1 {
+                parts.pop().unwrap()
+            } else {
+                "No Title"
+            };
+    let songs = parts.into_iter()
+            .take(400)   // <<< TODO: remove
+            .map(Song::from_text)
+            .collect();
+    Ok(Collection{
+        title: collection_title.to_string(),
+        songs})
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::branch::Alt;
+
+    use crate::tokenize::AlteredNotes;
+
+    use super::*;
+
+    #[test]
+    fn work() {
+        let text = "irealb://Work=Monk%20Thelonious==Medium%20Swing=Db==1r34LbK\
+                cu7KQyX74Db7X7bEZL7E%207FZL%20lKcQyX7bGZL%20lcKQyXyQ%7CD4TA%2A%7B7F%7CQy%5\
+                B%2ABD7L%20lcKQyX5b7C%7CQXy5b7GZL5b7G%20susZCh7X%7D%20%20lcFZL%20l7%20A7L7\
+                bGZL%20lcKQyX7bCD%2A%5B%5DQyX5%239b7bAZXyQKcE%7CQyX7%20E7LZEb7XyQ%7CD7XyQK\
+                cl%20Q%20ZY%7CQGXyQZ%20==0=0===";
+        let result = parse_url(text).unwrap();
+
+        #[allow(non_snake_case)]
+        let Ab7b9s5 = BarElement::Chord(
+            Chord {
+                root: Note::AFlat,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![AlteredNotes::Flat(Number::Nine), AlteredNotes::Sharp(Number::Five)],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let A7 = BarElement::Chord(
+            Chord {
+                root: Note::A,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let C7b5 = BarElement::Chord(
+            Chord {
+                root: Note::C,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![AlteredNotes::Flat(Number::Five)],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let Ch7= BarElement::Chord(
+            Chord {
+                root: Note::C,
+                flavor: Flavor::HalfDiminished(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let Db7 = BarElement::Chord(
+            Chord {
+                root: Note::DFlat,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let D7 = BarElement::Chord(
+            Chord {
+                root: Note::D,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let D7sus = BarElement::Chord(
+            Chord {
+                root: Note::D,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![AlteredNotes::Sus],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let Eb7 = BarElement::Chord(
+            Chord {
+                root: Note::EFlat,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let E7 = BarElement::Chord(
+            Chord {
+                root: Note::E,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let F7 = BarElement::Chord(
+            Chord {
+                root: Note::F,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let Gb7 = BarElement::Chord(
+            Chord {
+                root: Note::GFlat,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let G = BarElement::Chord(
+            Chord {
+                root: Note::G,
+                flavor: Flavor::Dominant(None),
+                altered_notes: vec![],
+                bass_note: None
+            }
+        );
+
+        #[allow(non_snake_case)]
+        let G7b5 = BarElement::Chord(
+            Chord {
+                root: Note::G,
+                flavor: Flavor::Dominant(Some(Number::Seven)),
+                altered_notes: vec![AlteredNotes::Flat(Number::Five)],
+                bass_note: None
+            }
+        );
+
+        assert_eq!(result, Collection {
+            title: String::new(),
+            songs: vec![
+                Song {
+                    title: "Work".to_string(),
+                    composer: "Monk Thelonious".to_string(),
+                    style: "Medium Swing".to_string(),
+                    key: "Db".to_string(),
+                    transpose: "".to_string(),
+                    music: Music {
+                        repeat_start: None,
+                        raw: "{*AT44Db7XyQKcl LZGb7XyQKcl LZF7 E7LZEb7XyQ|D7XyQKcl  }\
+                            [*BD7sus G7b5LZG7b5XyQ|C7b5XyQKcl LZCh7XyQ|F7XyQ|E7 A7LZAb7b9#5XyQ]\
+                            [*CDb7XyQKcl LZGb7XyQKcl LZF7 E7LZEb7XyQ|D7XyQKcl Q ZY|QGXyQZ ".to_string(),
+                        bars: vec![
+                            Bar { elements: vec![Db7.clone()] },
+                            Bar { elements: vec![Db7.clone()] },
+                            Bar { elements: vec![Gb7.clone()] },
+                            Bar { elements: vec![Gb7.clone()] },
+                            Bar { elements: vec![F7.clone(), E7.clone()] },
+                            Bar { elements: vec![Eb7.clone()] },
+                            Bar { elements: vec![D7.clone()] },
+                            Bar { elements: vec![D7.clone()] },
+                            Bar { elements: vec![D7sus.clone(), G7b5.clone()] },
+                            Bar { elements: vec![G7b5.clone()] },
+                            Bar { elements: vec![C7b5.clone()] },
+                            Bar { elements: vec![C7b5.clone()] },
+                            Bar { elements: vec![Ch7.clone()] },
+                            Bar { elements: vec![F7.clone()] },
+                            Bar { elements: vec![E7.clone(), A7.clone()] },
+                            Bar { elements: vec![Ab7b9s5.clone()] },
+                            Bar { elements: vec![Db7.clone()] },
+                            Bar { elements: vec![Db7.clone()] },
+                            Bar { elements: vec![Gb7.clone()] },
+                            Bar { elements: vec![Gb7.clone()] },
+                            Bar { elements: vec![F7.clone(), E7.clone()] },
+                            Bar { elements: vec![Eb7.clone()] },
+                            Bar { elements: vec![D7.clone()] },
+                            Bar { elements: vec![D7.clone()] },
+                            Bar { elements: vec![] },
+                            Bar { elements: vec![G.clone()] },
+                    ]
+            }, comp_style: "".to_string(), bpm: 0, repeats: "0".to_string() }]
+        });
+    }
+}
---
+++ src/tokenize.rs
@@ -0,0 +1,475 @@
+use std::fmt;
+
+use nom::IResult;
+use nom::bytes::complete::take;
+use nom::bytes::complete::tag;
+use nom::bytes::complete::take_until;
+use nom::character::complete::digit1;
+use nom::combinator::all_consuming;
+use nom::sequence::tuple;
+use nom::branch::alt;
+use nom::combinator::map;
+use nom::multi::many0;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Bar {
+    pub elements : Vec<BarElement>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Music {
+    pub repeat_start : Option<usize>,
+    pub raw : String,
+    pub bars : Vec<Bar>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BarElement {
+    SectionMarker(String),
+    TimeSignature(u32, u32),
+    Chord(Chord)
+}
+
+impl fmt::Display for BarElement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BarElement::SectionMarker(s) => s.fmt(f),
+            BarElement::TimeSignature(n, d) => format!("{}:{}", n, d).fmt(f),
+            BarElement::Chord(c) => c.fmt(f)
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct Chord {
+    pub root : Note,
+    pub flavor : Flavor,
+    pub altered_notes : Vec<AlteredNotes>,
+    pub bass_note : Option<Note>
+}
+
+impl Chord {
+    pub fn basic(root: Note, flavor: Flavor) -> Self {
+        Chord {
+            root,
+            flavor,
+            altered_notes : vec![],
+            bass_note : None,
+        }
+    }
+}
+
+impl fmt::Display for Chord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}{}{}",
+            self.root,
+            self.flavor,
+            self.altered_notes.iter().map(|x| x.to_string()).collect::<String>(),
+            match &self.bass_note {
+                Some(note) => format!("/{}", note),
+                None => "".to_string()
+            }
+        )
+    }
+}
+
+impl fmt::Debug for Chord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        format!("{}", self).fmt(f)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Token {
+    Alternate(String), // These show up above the regular music.
+    Bar,
+    Blank,
+    Chord(Chord),
+    Coda,
+    Comma,
+    Comment(String), // There is some stuff inside the comment that we could probably parse.
+    DoubleBarEnd,
+    DoubleBarStart,
+    EndingMeasure,
+    FinalBar,
+    NoChord,
+    NumberedEnding(String),
+    PauseSlash,
+    RepeatEnd,
+    RepeatMeasure,
+    BarAndRepeat,
+    RepeatTwoMeasures,
+    RepeatStart,
+    SectionMarker(String),
+    Segno,
+    TimeSignature(u32, u32),
+    VerticalSpace,
+
+    // Seems to be used when there is one chord per note.
+    Squeeze,
+
+    // TODO: What are these for?
+    SmallL,
+    SmallF,
+    Space
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Note {
+    AFlat, A, ASharp,
+    BFlat, B,
+    CFlat, C, CSharp,
+    DFlat, D, DSharp,
+    EFlat, E,
+    F, FSharp,
+    GFlat, G, GSharp,
+    // Indicates no note? Used as in "W/C" which visually just shows "/C"
+    W
+}
+
+impl fmt::Display for Note {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Note::AFlat => "Ab",
+            Note::A => "A",
+            Note::ASharp => "A#",
+            Note::BFlat => "Bb",
+            Note::B => "B",
+            Note::CFlat => "Cb",
+            Note::C => "C",
+            Note::CSharp => "C#",
+            Note::DFlat => "Db",
+            Note::D => "D",
+            Note::DSharp => "D#",
+            Note::EFlat => "Eb",
+            Note::E => "E",
+            Note::F => "F",
+            Note::FSharp => "F#",
+            Note::GFlat => "Gb",
+            Note::G => "G",
+            Note::GSharp => "G#",
+            Note::W => "W"
+        }.fmt(f)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Number {
+    Two, Three, Five, Six, Seven, Nine, Eleven, Thirteen
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Number::Two => "2",
+            Number::Three => "3",
+            Number::Five => "5",
+            Number::Six => "6",
+            Number::Seven => "7",
+            Number::Nine => "9",
+            Number::Eleven => "11",
+            Number::Thirteen => "13"
+        }.fmt(f)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Flavor {
+    Augmented(Option<Number>),
+    Diminished(Option<Number>),
+    DiminishedMajor(Option<Number>),
+    HalfDiminished(Option<Number>),
+    Minor(Option<Number>),
+    MinorMajor(Option<Number>),
+    Dominant(Option<Number>),
+    Major(Option<Number>),
+    SixthNinth,
+    MinorSixthNinth
+}
+
+impl fmt::Display for Flavor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Flavor::Augmented(n) => match n {
+                Some(n) => format!("+{}", n),
+                None => "+".to_string()
+            },
+            Flavor::Diminished(n) => match n {
+                Some(n) => format!("o{}", n),
+                None => "o".to_string()
+            },
+            Flavor::DiminishedMajor(n) => match n {
+                Some(n) => format!("o^{}", n),
+                None => "o^".to_string()
+            },
+            Flavor::HalfDiminished(n) => match n {
+                Some(n) => format!("h{}", n),
+                None => "h".to_string()
+            },
+            Flavor::Minor(n) => match n {
+                Some(n) => format!("-{}", n),
+                None => "-".to_string()
+            },
+            Flavor::MinorMajor(n) => match n {
+                Some(n) => format!("-^{}", n),
+                None => "-^".to_string()
+            },
+            Flavor::Dominant(n) => match n {
+                Some(n) => format!("{}", n),
+                None => "".to_string()
+            },
+            Flavor::Major(n) => match n {
+                Some(n) => format!("^{}", n),
+                None => "^".to_string()
+            },
+            Flavor::SixthNinth => "69".to_string(),
+            Flavor::MinorSixthNinth => "m69".to_string()
+        }.fmt(f)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum AlteredNotes {
+    Flat(Number),
+    Sharp(Number),
+    Add(Number),
+    Sus,
+    Alt
+}
+
+impl fmt::Display for AlteredNotes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AlteredNotes::Flat(n) => format!("b{}", n),
+            AlteredNotes::Sharp(n) => format!("#{}", n),
+            AlteredNotes::Add(n) => format!("add{}", n),
+            AlteredNotes::Sus => "sus".to_string(),
+            AlteredNotes::Alt => "alt".to_string()
+        }.fmt(f)
+    }
+}
+
+fn section_marker<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    map(tuple((tag("*"), take(1usize))),
+        |x : (&str, &str)| Token::SectionMarker(x.1.to_string()))
+}
+
+fn numbered_ending<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    map(tuple((tag("N"), take(1usize))),
+        |x : (&str, &str)| Token::NumberedEnding(x.1.to_string()))
+}
+
+fn comment<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    map(tuple((tag("<"), take_until(">"), tag(">"))),
+        |x : (&str, &str, &str)| Token::Comment(x.1.to_string()))
+}
+
+fn alternate<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    map(tuple((tag("("), take_until(")"), tag(")"))),
+        |x : (&str, &str, &str)| Token::Alternate(x.1.to_string()))
+}
+
+fn note<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Note> {
+    // Put longer strings first.
+    alt((
+        map(tag("Ab"), |_| Note::AFlat),
+        map(tag("A#"), |_| Note::ASharp),
+        map(tag("A"), |_| Note::A),
+        map(tag("Bb"), |_| Note::BFlat),
+        map(tag("B"), |_| Note::B),
+        map(tag("Cb"), |_| Note::CFlat),
+        map(tag("C#"), |_| Note::CSharp),
+        map(tag("C"), |_| Note::C),
+        map(tag("Db"), |_| Note::DFlat),
+        map(tag("D#"), |_| Note::DSharp),
+        map(tag("D"), |_| Note::D),
+        map(tag("Eb"), |_| Note::EFlat),
+        map(tag("E"), |_| Note::E),
+        map(tag("F#"), |_| Note::FSharp),
+        map(tag("F"), |_| Note::F),
+        map(tag("Gb"), |_| Note::GFlat),
+        map(tag("G#"), |_| Note::GSharp),
+        map(tag("G"), |_| Note::G),
+        map(tag("W"), |_| Note::W)
+    ))
+}
+
+fn number<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Number> {
+    alt((
+        map(tag("2"), |_| Number::Two),
+        map(tag("3"), |_| Number::Three),
+        map(tag("5"), |_| Number::Five),
+        map(tag("6"), |_| Number::Six),
+        map(tag("7"), |_| Number::Seven),
+        map(tag("9"), |_| Number::Nine),
+        map(tag("11"), |_| Number::Eleven),
+        map(tag("13"), |_| Number::Thirteen)
+    ))
+}
+
+fn number_option<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Option<Number>> {
+    alt((
+        map(number(), |x| Some(x)),
+        map(tag(""), |_| None),
+    ))
+}
+
+fn flavor<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Flavor> {
+    // Put longer strings first.
+    alt((
+        map(tag("69"), |_| Flavor::SixthNinth),
+        map(tag("-69"), |_| Flavor::MinorSixthNinth),
+        map(tuple((tag("-^"), number_option())), |x| Flavor::MinorMajor(x.1)),
+        map(tuple((tag("-"), number_option())), |x| Flavor::Minor(x.1)),
+        map(tuple((tag("^"), number_option())), |x| Flavor::Major(x.1)),
+        map(tuple((tag("h"), number_option())), |x| Flavor::HalfDiminished(x.1)),
+        map(tuple((tag("o^"), number_option())), |x| Flavor::DiminishedMajor(x.1)),
+        map(tuple((tag("o"), number_option())), |x| Flavor::Diminished(x.1)),
+        map(tuple((tag("+"), number_option())), |x| Flavor::Augmented(x.1)),
+        map(number_option(), |x| Flavor::Dominant(x))
+    ))
+}
+
+fn altered_notes<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Vec<AlteredNotes>> {
+    many0(
+        alt((
+            map(tuple((tag("b"), number())), |x| AlteredNotes::Flat(x.1)),
+            map(tuple((tag("#"), number())), |x| AlteredNotes::Sharp(x.1)),
+            map(tuple((tag("add"), number())), |x| AlteredNotes::Add(x.1)),
+            map(tag("sus"), |_| AlteredNotes::Sus),
+            map(tag("alt"), |_| AlteredNotes::Alt)
+        ))
+    )
+}
+
+fn over<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Option<Note>> {
+    alt((
+        map( tuple((tag("/"), note())), |x| Some(x.1)),
+        map( tag(""), |_| None)
+    ))
+}
+
+fn chord<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    alt((
+        map( tag("n"), |_| Token::NoChord),
+        map(tuple((note(), flavor(), altered_notes(), over())),
+            |x| Token::Chord(
+                Chord {
+                    root: x.0,
+                    flavor: x.1,
+                    altered_notes: x.2,
+                    bass_note: x.3
+                }))
+    ))
+}
+
+fn time_signature<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Token> {
+    /* The only signatures in jazz1400 are: T24, T34, T44, T54, T64. */
+    /* Assume top number can be multiple digits, and the bottom number is a
+     * single digit. */
+    map(tuple((tag("T"), digit1)),
+        |x| {
+            let digits : &str = x.1;
+            let (top, bottom) = digits.split_at(digits.len() - 1);
+            let top_num = top.parse().unwrap();
+            let bottom_num = bottom.parse().unwrap();
+            Token::TimeSignature(top_num, bottom_num)
+    })
+}
+
+fn control<'a>() -> impl FnMut(&'a str) -> IResult<&str, Token>
+{
+    alt((
+            map(tag("{"), |_| Token::RepeatStart),
+            map(tag("}|"), |_| Token::RepeatEnd),
+            map(tag("}"), |_| Token::RepeatEnd),
+            map(tag(","), |_| Token::Comma),
+            map(tag("XyQ"), |_| Token::Blank),
+            map(tag("r|"), |_| Token::RepeatTwoMeasures),
+            map(tag("x"), |_| Token::RepeatMeasure),
+            map(tag("s"), |_| Token::Squeeze),
+            map(tag("Q"), |_| Token::Coda),
+            map(tag("S"), |_| Token::Segno),
+            map(tag("Y"), |_| Token::VerticalSpace),
+            map(tag("p"), |_| Token::PauseSlash),
+            map(tag("U"), |_| Token::EndingMeasure),
+            map(tag("l"), |_| Token::SmallL),
+            map(tag("f"), |_| Token::SmallF),
+            map(tag(" "), |_| Token::Space),
+    ))
+}
+
+fn bar<'a>() -> impl FnMut(&'a str) -> IResult<&str, Token>
+{
+    alt((
+            map(tag("||"), |_| Token::Bar),
+            map(tag("|"), |_| Token::Bar),
+            map(tag("["), |_| Token::DoubleBarStart),
+            map(tag("]"), |_| Token::DoubleBarEnd),
+            map(tag("Z"), |_| Token::FinalBar),
+            map(tag("Kcl"), |_| Token::BarAndRepeat),
+            map(tag("LZ|"), |_| Token::Bar),
+            map(tag("LZ"), |_| Token::Bar),
+    ))
+}
+
+fn parse_tokens(input : &str) -> IResult<&str, Vec<Token>>
+{
+    many0(
+        alt((
+            chord(),
+            bar(),
+            control(),
+            comment(),
+            alternate(),
+            section_marker(),
+            numbered_ending(),
+            time_signature()
+        ))
+    )(input)
+}
+
+pub fn tokenize_music(text : &str) -> Result<Music, String>
+{
+    let mut bars = vec![];
+    let mut bar = Bar{elements : vec![]};
+    for token in all_consuming(parse_tokens)(text).unwrap().1 {
+        match token {
+            Token::Bar | Token::FinalBar | Token::DoubleBarEnd | Token::RepeatEnd => {
+                bars.push(bar);
+                bar = Bar{elements : vec![]};
+            },
+            Token::Chord(c) => {
+                bar.elements.push(BarElement::Chord(c));
+            },
+            Token::RepeatMeasure => {
+                if bars.len() == 0 {
+                    return Err("Repeat measure at beginning of song".to_string());
+                }
+                let last_bar = bars.last().unwrap();
+                bar = last_bar.clone();
+            },
+            Token::RepeatTwoMeasures => {
+                if bars.len() < 2 {
+                    return Err("Repeat 2 measures at beginning of song".to_string());
+                }
+                let a = bars[bars.len() - 2].clone();
+                let b = bars[bars.len() - 1].clone();
+                bars.push(a);
+                bar = b.clone()
+            },
+            Token::BarAndRepeat => {
+                bars.push(bar);
+                let last_bar = bars.last().unwrap();
+                bar = last_bar.clone();
+            },
+            // Ignore other tokens for now
+            _ => ()
+        }
+    }
+    Ok(Music {
+        repeat_start : None,
+        bars,
+        raw : text.to_string()
+    })
+}
